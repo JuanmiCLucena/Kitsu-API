5be0095ef1c4af65bdd6a8dc06f136e662d46a45
Juanmi <jmcostalucena2000@gmail.com>
2022-05-18 16:29:20 +0200

    Cambiado footer

--- templates/navbar.php
+++ templates/navbar.php
@@ -7,7 +7,7 @@ $activePage = basename($_SERVER['PHP_SELF'], ".php");
 <!-- Navbar -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 	<div class="container-fluid">
-	<a class="navbar-brand fs-1 fw-bold" href="index.php"><img src="assets/images/logonav.jpg" width="100px" alt="Anime logo">KITSU</a>
+	<a class="navbar-brand fs-1 fw-bold" href="https://kitsu.docs.apiary.io/#"><img src="assets/images/logonav.jpg" width="100px" alt="Anime logo">KITSU</a>
 	
 		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
