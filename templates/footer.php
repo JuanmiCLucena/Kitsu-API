01fd49f48cc6bab4e2a99a50c267157223096296
Juanmi <jmcostalucena2000@gmail.com>
2022-05-27 14:19:54 +0200

    Mejoras visuales

--- templates/footer.php
+++ templates/footer.php
@@ -1,10 +1,10 @@
 <footer class="text-center text-white fixed-bottom footer">
     <!-- Grid container -->
-    <div class="container p-3"></div>
+    <div class="container p-1"></div>
     <!-- Grid container -->
 
     <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+    <div class="text-center p-1">
         <p>
             Copyright &copy; 2022, Anime API. Designed by <a href="http://gettemplate.com/" rel="designer">gettemplate</a>. Product of <a href="https://pozhilov.com/">Sergey Pozhilov</a>. <a rel="license" href="//creativecommons.org/licenses/by/3.0/" class="license-button"><img alt="Creative Commons License" style="border-width:0" src="//i.creativecommons.org/l/by/3.0/88x31.png"></a>
             <br><a href='https://www.freepik.es/vectores/samurai'>Vector de samurai creado por catalyststuff - www.freepik.es</a>
