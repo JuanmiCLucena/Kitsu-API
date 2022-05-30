60b6f1e7ac7de468539af00c822fa3090f876818
Juanmi <jmcostalucena2000@gmail.com>
2022-05-30 19:39:33 +0200

    .

--- login.php
+++ login.php
@@ -15,7 +15,7 @@ require_once('CRUDUser.php');
 
 	<link rel="shortcut icon" href="assets/images/logo.webp">
 
-	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" media="screen" href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
 	<link rel="stylesheet" href="assets/css/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/css/font-awesome.min.css">
 
