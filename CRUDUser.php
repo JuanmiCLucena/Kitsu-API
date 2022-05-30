c3748eaaf504e856d0104e10bfcb70757dbf8ae0
Juanmi <jmcostalucena2000@gmail.com>
2022-05-30 12:11:30 +0200

    Anyadido validacion de registro de usuarios e insercion de animes

--- CRUDUser.php
+++ CRUDUser.php
@@ -22,10 +22,18 @@ function login()
 function insertar()
 {
     require_once('bd.php');
-    $sql = "INSERT INTO usuarios(usuario,nombre,apellidos, email, password) VALUES (:usuario,:nombre,:apellidos,:email,:password)";
-    $consulta = $bd->prepare($sql);
-    $consulta->execute(["usuario" => $_POST['usuario'], "nombre" => $_POST['nombre'], "apellidos" => $_POST['apellidos'], "email" => $_POST['email'], "password" => password_hash($_POST['contrasenya1'], PASSWORD_DEFAULT)]);
+    $query = "SELECT COUNT(*) FROM usuarios WHERE usuario = :usuario";
+    $stmt = $bd->prepare($query);
+    $stmt->execute(["usuario" => $_POST['usuario']]);
+    $numberRows = $stmt->fetchColumn();
+
+    if ($numberRows == 0) {
+        $sql = "INSERT INTO usuarios(usuario,nombre,apellidos, email, password) VALUES (:usuario,:nombre,:apellidos,:email,:password)";
+        $consulta = $bd->prepare($sql);
+        $consulta->execute(["usuario" => $_POST['usuario'], "nombre" => $_POST['nombre'], "apellidos" => $_POST['apellidos'], "email" => $_POST['email'], "password" => password_hash($_POST['contrasenya1'], PASSWORD_DEFAULT)]);
+    } else { ?>
+        <script>
+            alert('El usuario ya existe.');
+        </script>
+<?php }
 }
-
-
-
--- anadirfavorito.php
+++ anadirfavorito.php
@@ -1,9 +1,28 @@
 <?php
- if (isset($_POST['fav'])) {
-    require_once('bd.php');
-    $sql = "INSERT INTO animes_favoritos(idAnime,nombreAnime,idUsuario,nombreUsuario,enlaceAnime) VALUES (:idAnime,:nombreAnime,:idUsuario,:usuario,:enlaceAnime)";
-    $consulta = $bd->prepare($sql);
-    $consulta->execute(["idAnime" => $_POST['idAnime'],"nombreAnime" => $_POST['nombreAnime'], "idUsuario" => $_POST['idUsuario'],"usuario" => $_POST['nombreUsuario'],"enlaceAnime" => $_POST['enlaceAnime']]);
-    header("Location:favoritos.php");
- }
-?>
+session_start();
+if (isset($_POST['fav'])) {
+   require_once('bd.php');
+   $id = $_POST['idAnime'];
+   $query = "SELECT COUNT(*) FROM animes_favoritos WHERE idAnime = :idAnime AND idUsuario=" . $_SESSION['idUsuario'];
+   $stmt = $bd->prepare($query);
+   $stmt->bindParam(':idAnime', $id, PDO::PARAM_INT);
+   $stmt->execute();
+   $numberRows = $stmt->fetchColumn();
+
+   if ($numberRows == 0 ) {
+      $sql = "INSERT INTO animes_favoritos(idAnime,nombreAnime,idUsuario,nombreUsuario,enlaceAnime) VALUES (:idAnime,:nombreAnime,:idUsuario,:usuario,:enlaceAnime)";
+      $consulta = $bd->prepare($sql);
+      $consulta->execute(["idAnime" => $_POST['idAnime'], "nombreAnime" => $_POST['nombreAnime'], "idUsuario" => $_POST['idUsuario'], "usuario" => $_POST['nombreUsuario'], "enlaceAnime" => $_POST['enlaceAnime']]);
+      header("Location:favoritos.php");
+   } else { ?>
+      <script>
+         alert('El anime seleccionado ya se encuentra en la lista de favoritos.');
+         window.location.href='favoritos.php';
+      </script>
+      
+  <?php }
+
+   
+}
+
+
